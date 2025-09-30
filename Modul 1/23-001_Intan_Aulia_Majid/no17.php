0094b2ee1e99cf7698adbe2cd3f150534339cb79
IntanAuliaMajid <intaaan741@gmail.com>
2025-09-30 21:48:37 +0700

    modul1

---
+++ Modul 1/23-001_Intan_Aulia_Majid/no17.php
@@ -0,0 +1,11 @@
+<?php
+function sum($x, $y) {
+    echo "$x+$y = ";
+    echo $x+$y;
+    echo "<br>";
+}
+
+sum(5,10);
+sum(7,13);
+sum(2,4);
+?>
---
+++ Modul 1/23-001_Intan_Aulia_Majid/no3.php
@@ -0,0 +1,20 @@
+<?php
+    // ini non-embeded script 
+    echo "Hello world";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // ini embedded-script 
+    echo "Hello World" 
+    ?>
+</body>
+</html>
