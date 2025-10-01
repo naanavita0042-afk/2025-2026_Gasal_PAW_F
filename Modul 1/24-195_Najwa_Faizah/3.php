6e406624ff3a457c55dd30419a1ae0d85d86982a
Najwfazh <akunbelajarjua@gmail.com>
2025-10-01 10:09:38 +0700

    Add files via upload

---
+++ Modul 1/24-195_Najwa_Faizah/3.php
@@ -0,0 +1,17 @@
+<!-- soal 1 -->
+<?php
+    echo "Hello world <br>"; // ini non-embeded script
+?>
+
+<!-- soal 2 -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <?php echo "Hello world"; //ini embedded-script ?>  
+ </body>
+ </html>
